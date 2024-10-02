44332654a55fe45d935995b95cf5b6b64f833d45
devsamuellgc <samuelgalrao@hotmail.com>
2024-10-01 21:50:44 -0300

    aula inicial de cypress

---
+++ react/aula-rotas/cypress/e2e/Login.cy.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+
+describe("Login feature", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5173/login");
+  });
+
+  it("should be able to type on inputs", () => {});
+
+  it("should be able to login on the system", () => {})
+
+  it("should login successfully", () => {})
+});
